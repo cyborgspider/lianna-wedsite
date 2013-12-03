bb193796d39b06ea13832a33e1fded50ce4d4f7d
Neil Soult <nsoult@greendotcorp.com>
2013-12-02 20:59:10 -0800

    changed global styles to remove background from html element - BAD CARLOS! added scripts to dynamically change the rows in RSVP page based on number of guests

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -6,6 +6,27 @@
     $('.meal').tooltip({
       html: true
     });
+    $('#number').blur(function() {
+      var num, row, rows, _results;
+      num = $(this).val();
+      rows = $('.bot-form .row').length;
+      if (rows < num) {
+        _results = [];
+        while (num -= 1) {
+          row = $('.bot-form .row:first').clone();
+          row.find('.guestName').attr('name', function(i, a) {
+            return a + num;
+          });
+          row.find('.mealChoice').attr('name', function(i, a) {
+            return a + num;
+          });
+          _results.push(row.appendTo('.bot-form'));
+        }
+        return _results;
+      } else if (rows > num) {
+        return $(".bot-form .row:nth-child(" + num + ")").nextUntil().remove();
+      }
+    });
     return $('.meal').click(function() {
       $(this).addClass('chosen');
       return $(this).siblings().removeClass('chosen');
--- site/scripts/scripts.coffee
+++ site/scripts/scripts.coffee
@@ -4,6 +4,17 @@ $ ->
 
   $('.meal').tooltip({html:true})
 
+  $('#number').blur ->
+    num = $(@).val()
+    rows = $('.bot-form .row').length
+    if rows < num
+      while num -= 1
+        row = $('.bot-form .row:first').clone()
+        row.find('.guestName').attr('name', (i, a) -> return a + num)
+        row.find('.mealChoice').attr('name', (i, a) -> return a + num)
+        row.appendTo '.bot-form'
+    else if rows > num
+      $(".bot-form .row:nth-child(#{num})").nextUntil().remove() #fuck coffeescript
 
   $('.meal').click ->
     $(@).addClass 'chosen'
