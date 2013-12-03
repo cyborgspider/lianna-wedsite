93c72d2a0e05529f7dee3e25b02465c0d50c74ac
Neil Soult <nsoult@greendotcorp.com>
2013-12-03 00:03:08 -0800

    final for tonight: php email works minor design / css tweaks thank you page added changed to jade content blocks magic removed herobrine

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,33 +1,41 @@
 (function() {
   $(function() {
+    if ($("li a[href='" + (location.pathname.slice(1)) + "']").length) {
+      $("li a[href='" + (location.pathname.slice(1)) + "']").addClass('active');
+    }
     $('#logo').click(function() {
-      return window.location.href = "index.html";
+      return window.location.href = "/";
     });
-    $('.meal').tooltip({
-      html: true
-    });
+    if ($('.meal').length) {
+      $('.meal').tooltip({
+        html: true
+      });
+    }
     $('#number').blur(function() {
-      var num, row, rows, _results;
+      var first, num, row, rows, _results;
       num = $(this).val();
       rows = $('.bot-form .row').length;
+      first = $('.bot-form .row:first');
       if (rows < num) {
         _results = [];
         while (num -= 1) {
-          row = $('.bot-form .row:first').clone();
+          row = first.clone();
           row.find('.guestName').attr('name', function(i, a) {
-            return a + num;
-          });
+            return a.replace("[0]", "[" + num + "]");
+          }).val('');
+          row.find('.meal').removeClass('chosen');
           row.find('.mealChoice').attr('name', function(i, a) {
-            return a + num;
-          });
-          _results.push(row.appendTo('.bot-form'));
+            return a.replace("[0]", "[" + num + "]");
+          }).val('');
+          _results.push(first.after(row));
         }
         return _results;
       } else if (rows > num) {
         return $(".bot-form .row:nth-child(" + num + ")").nextUntil().remove();
       }
     });
-    return $('.meal').click(function() {
+    return $('.bot-form').on('click', '.meal', function() {
+      $(this).parent().siblings('.mealChoice').val($(this).data('meal'));
       $(this).addClass('chosen');
       return $(this).siblings().removeClass('chosen');
     });
--- site/scripts/scripts.coffee
+++ site/scripts/scripts.coffee
@@ -1,22 +1,27 @@
 $ ->
-  $('#logo').click ->
-    window.location.href = "index.html"
+  $("li a[href='#{location.pathname.slice(1)}']").addClass('active') if $("li a[href='#{location.pathname.slice(1)}']").length
 
-  $('.meal').tooltip({html:true})
+  $('#logo').click ->
+    window.location.href = "/"
+
+  $('.meal').tooltip({html:true}) if $('.meal').length
 
   $('#number').blur ->
     num = $(@).val()
     rows = $('.bot-form .row').length
+    first = $('.bot-form .row:first')
     if rows < num
       while num -= 1
-        row = $('.bot-form .row:first').clone()
-        row.find('.guestName').attr('name', (i, a) -> return a + num)
-        row.find('.mealChoice').attr('name', (i, a) -> return a + num)
-        row.appendTo '.bot-form'
+        row = first.clone()
+        row.find('.guestName').attr('name', (i, a) -> return a.replace("[0]", "[#{num}]")).val ''
+        row.find('.meal').removeClass 'chosen'
+        row.find('.mealChoice').attr('name', (i, a) -> return a.replace("[0]", "[#{num}]")).val ''
+        first.after row
     else if rows > num
-      $(".bot-form .row:nth-child(#{num})").nextUntil().remove() #fuck coffeescript
+      $(".bot-form .row:nth-child(#{num})").nextUntil().remove()
 
-  $('.meal').click ->
+  $('.bot-form').on 'click', '.meal', () ->
+    $(@).parent().siblings('.mealChoice').val $(@).data 'meal'
     $(@).addClass 'chosen'
     $(@).siblings().removeClass 'chosen'
 
